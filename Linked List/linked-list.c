74a7c42fd0679e45e1267b1132fcef4e8869fc2c
Jean Thome <j146562@g.unicamp.br>
2018-10-11 22:52:20 -0300

    Adicionadas novas features - Alterada estrutura para a lista ser duplamente ligada (*previous e *next). - get_first() - get_last() - remove_first() - remove_last() - reverse_print()

--- Linked List/linked-list.c
+++ Linked List/linked-list.c
@@ -6,6 +6,7 @@
 */
 typedef struct Node {
   int value;                /* Valor salvo no nó. */
+  struct Node * previous;   /* Ponteiro para o elemento anterior. */
   struct Node * next;       /* Ponteiro para o próximo elemento da lista. */
 } Node;
 
@@ -21,17 +22,32 @@ typedef struct LinkedList {
 LinkedList * build_linked_list();
 Node * get_node();
 LinkedList * add(LinkedList * l, int value);
-LinkedList * addAtBeginning(LinkedList * l, int value);
+LinkedList * add_at_beginning(LinkedList * l, int value);
+LinkedList * remove_first(LinkedList *l);
+LinkedList * remove_last(LinkedList *l);
+
+int get_first(LinkedList *l);
+int get_last(LinkedList *l);
 void print(LinkedList * l);
+void reverse_print(LinkedList *l);
 
 int main(int argc, char const *argv[]) { 
   
   LinkedList *list = build_linked_list();
 
-  for (int i = 0; i < 20; i++) {
-    list = addAtBeginning(list, i);
+  for (int i = 1; i <= 10; i++) {
+    list = add(list, i);
   }
+
   print(list);
+  reverse_print(list);
+
+  list = remove_first(list);
+  list = remove_last(list);
+
+  print(list);
+  reverse_print(list);
+
   return 0;
 }
 
@@ -53,6 +69,7 @@ LinkedList * build_linked_list () {
 */
 Node * get_node() {
   Node * node = (Node *)malloc(sizeof(Node));
+  node->previous = NULL;
   node->next = NULL;
   return node;
 }
@@ -73,6 +90,7 @@ LinkedList * add(LinkedList * l, int value) {
     l->head = new;
     l->tail = new;
   } else {
+    new->previous = l->tail;
     l->tail->next = new;
     l->tail = new;
   }
@@ -87,7 +105,7 @@ LinkedList * add(LinkedList * l, int value) {
 * @param value O valor a ser adicionado.
 * @return Ponteiro para a lista onde o novo elemento foi adicionado.
 */
-LinkedList * addAtBeginning(LinkedList * l, int value) {
+LinkedList * add_at_beginning(LinkedList * l, int value) {
   Node *node, *new;
   
   new = get_node();
@@ -98,6 +116,7 @@ LinkedList * addAtBeginning(LinkedList * l, int value) {
     l->tail = new;
   } else {
     new->next = l->head;
+    l->head->previous = new;
     l->head = new;
   }
   
@@ -106,19 +125,85 @@ LinkedList * addAtBeginning(LinkedList * l, int value) {
 }
 
 /**
-* Imprime o tamanho da lista e seus respectivos elementos.
+* Remove o primeiro elemento da lista;
+* @param l Ponteiro para a lista.
+* return Ponteiro para a lista atualizada.
+*/
+LinkedList * remove_first(LinkedList *l) {
+
+  if (l->size > 0) {
+    Node *first = l->head;
+    l->head->next->previous = NULL;
+    l->head = l->head->next;
+    free(first);
+    l->size--;
+  }
+
+  return l;
+}
+
+/**
+* Remove o último elemento da lista;
+* @param l Ponteiro para a lista.
+* return Ponteiro para a lista atualizada.
+*/
+LinkedList * remove_last(LinkedList *l) {
+  
+  if (l->size > 0) {
+    Node * last = l->tail;
+    l->tail->previous->next = NULL;
+    l->tail = l->tail->previous;
+    free(last);
+    l->size--;
+  }
+  return l;
+}
+
+/**
+* Obtém o valor armazenado no primeiro nó da lista.
+* @param l Ponteiro para a lista.
+* @return int com o valor no primeiro nó da lista.
+*/
+int get_first(LinkedList *l) {
+  return l->head->value;
+}
+
+/**
+* Obtém o valor armazenado no último nó da lista.
+* @param l Ponteiro para a lista.
+* @return int com o valor no último nó da lista.
+*/
+int get_last(LinkedList *l) {
+  return l->tail->value;
+}
+
+/**
+* Imprime o tamanho da lista e seus respectivos elementos, do começo para o final.
 * @param l Ponteiro para a lista a ser impressa. 
 */
 void print(LinkedList * l)  {
   Node * node = l->head;
 
-  printf("Size: %d\n", l->size);
+  printf("Size: %d : ", l->size);
   while (node != NULL) {
-    printf(" %d", node->value);
+    printf("<- %d ->", node->value);
     node = node->next;
   }
-  printf("\n");
+  printf("\n\n");
 }
 
 
+/**
+* Imprime o tamanho da lista e seus respectivos elementos, do final para o começo.
+* @param l Ponteiro para a lista a ser impressa.
+*/
+void reverse_print(LinkedList *l) {
+  Node * node = l->tail;
+  printf("Size: %d : ", l->size);
 
+  while(node != NULL) {
+    printf("<- %d ->", node->value);
+    node = node->previous;
+  }
+  printf("\n\n");
+}
